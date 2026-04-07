a31388521c7a469fdf953d27ecfda89d20fb5a39
Oscar Jovani <oscar.jovani@chillngointernational.com>
2026-04-06 23:03:59 -0600

    fix: explicit Supabase auth persistence config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://jahnlhzbjcbmjnuzxsvj.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphaG5saHpiamNibWpudXp4c3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NDQ0NTksImV4cCI6MjA4NzIyMDQ1OX0.KMuwdmjL4iTnFa_h9nVtfDrSFClVdcBhttXreEyq7aA'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+        storage: window.localStorage,
+        storageKey: 'cng-auth',
+        flowType: 'pkce',
+    },
+})
